583ee186de236857c96f65421dae212ca91d4c6f
Michael Fiford <michaelfiford@gmail.com>
2014-08-20 01:00:25 +0200

    Forgot to update cookie config

--- lib/LanWebsite/Auth/Default.php
+++ lib/LanWebsite/Auth/Default.php
@@ -42,7 +42,7 @@
 			$this->sessionId = uniqid($user->getUserId() . '.', true);
             $expires = time()+60*60*24*30;
 			LanWebsite_Main::getDb()->query("INSERT INTO `sessions` (session_id, user_id, expires) VALUES ('%s', '%s', '%s')", $this->sessionId, $user->getUserId(), $expires);
-            setcookie('lan_session', $this->sessionId, $expires, '/', $config['cookie']['hostname']);
+            setcookie('lan_session', $this->sessionId, $expires, '/', COOKIE_HOST);
 			
 			return true;
 		}
